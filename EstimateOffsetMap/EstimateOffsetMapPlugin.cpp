cfc79e41db3d4e5113b6bdbdb30592cd100e253d
Jonathan Hearn <jonathan.hearn@ajptechnical.com>
2022-01-01 19:45:01 +0000

    [EstimateOffsetMap] keeps offset inline by checking whether affected quadrangles are valid. Totally doesn't work, really

--- EstimateOffsetMap/EstimateOffsetMapPlugin.cpp
+++ EstimateOffsetMap/EstimateOffsetMapPlugin.cpp
@@ -58,6 +58,32 @@ inline double toRadians(double euc) {
     return M_PI * euc / 180.0;
 }
 
+OfxPointD pItoD(OfxPointI& p) {
+    OfxPointD pD;
+    pD.x = p.x;
+    pD.y = p.y;
+    return pD;
+}
+
+template<class T>
+inline T* pixelAddress(T* start, const OfxRectI* rod, int comps, int x, int y) {
+    x = std::max(std::min(x, rod->x2 - 1), rod->x1);
+    y = std::max(std::min(y, rod->y2 - 1), rod->y1);
+    auto offset = (y - rod->y1) * (rod->x2 - rod->x1) + (x - rod->x1);
+    return start + offset * comps;
+}
+
+inline bool operator<(const OfxPointI& lhs, const OfxPointI& rhs) {
+    return lhs.x < rhs.x || lhs.x == rhs.x && lhs.y < rhs.y;
+}
+
+inline OfxPointD operator/(const OfxPointD& p, double d) {
+    OfxPointD quot;
+    quot.x = p.x / d;
+    quot.y = p.y / d;
+    return quot;
+}
+
 void EstimateOffsetMapPlugin::render(const OFX::RenderArguments &args)
 {
     auto_ptr<Image> srcImg(_srcClip->fetchImage(args.time));
@@ -99,21 +125,16 @@ void EstimateOffsetMapPlugin::render(const OFX::RenderArguments &args)
     // initialise offsets & differences
     auto_ptr<float> draws(new float[rndNumPixels * 2]);
     auto_ptr<float> diffs(new float[rndNumPixels]);
-    auto_ptr<bool> changed(new bool[rndNumPixels]);
     auto drawsPtr = draws.get();
     auto diffPtr = diffs.get();
-    auto changedPtr = changed.get();
     float* trgPix;
     for (auto y=args.renderWindow.y1; y < args.renderWindow.y2; y++) {
         for (auto x=args.renderWindow.x1; x < args.renderWindow.x2; x++
-            ,drawsPtr += 2, diffPtr++, changedPtr++)
+            ,drawsPtr += 2, diffPtr++)
         {
             if (abort()) {return;}
-            *changedPtr = false;
-            for (auto c=0; c < 2; c++) {
-                drawsPtr[0] = x;
-                drawsPtr[1] = y;
-            }
+            drawsPtr[0] = x;
+            drawsPtr[1] = y;
             float* srcPix;
             if (blackOutside) {
                 srcPix = (float*)srcImg->getPixelAddress(x, y);
@@ -140,6 +161,11 @@ void EstimateOffsetMapPlugin::render(const OFX::RenderArguments &args)
     std::uniform_real_distribution<double> distrTransDir(0, M_PI * 2);
     std::uniform_real_distribution<double> distrSrcX(srcROD.x1, srcROD.x2);
     std::uniform_real_distribution<double> distrSrcY(srcROD.y1, srcROD.y2);
+
+    std::vector<OfxPointI> touched;
+    std::set<OfxPointI> touchedSet;
+    std::set<int> locked;
+    Quadrangle quad;
 
     auto_ptr<float> scratchDraws(new float[rndNumPixels * 2]);
     auto_ptr<float> scratchDiffs(new float[rndNumPixels]);
@@ -181,15 +207,19 @@ void EstimateOffsetMapPlugin::render(const OFX::RenderArguments &args)
             ,args.renderWindow.y2
         );
 
+        touched.clear();
+        touchedSet.clear();
+
         double oldScore = 0;
         double newScore = 0;
         auto_ptr<float> srcPix(new float[comps]);
-        for (auto y=bounds.y1; y < bounds.y2; y++) {
+        OfxPointI p;
+        for (p.y=bounds.y1; p.y < bounds.y2; p.y++) {
             if (abort()) {return;}
-            for (auto x=bounds.x1; x < bounds.x2; x++) {
+            for (p.x=bounds.x1; p.x < bounds.x2; p.x++) {
                 // relative to translated centre
-                auto dx = x - trgCentreX;
-                auto dy = y - trgCentreY;
+                auto dx = p.x - trgCentreX;
+                auto dy = p.y - trgCentreY;
                 // undo scale
                 dx /= scaleX;
                 dy /= scaleY;
@@ -204,17 +234,16 @@ void EstimateOffsetMapPlugin::render(const OFX::RenderArguments &args)
                 // set to draw from this src point
                 auto srcX = centreX + rotX;
                 auto srcY = centreY + rotY;
-                auto offset = rndWidth * (y - args.renderWindow.y1) + (x - args.renderWindow.x1);
-                changedPtr = changed.get() + offset;
-                *changedPtr = true;
-                scratchDrawPtr = scratchDraws.get() + offset * 2;
+                touched.push_back(p);
+                touchedSet.insert(p);
+                scratchDrawPtr = pixelAddress(scratchDraws.get(), &args.renderWindow, 2, p.x, p.y);
                 scratchDrawPtr[0] = srcX;
                 scratchDrawPtr[1] = srcY;
-                diffPtr = diffs.get() + offset;
+                diffPtr = pixelAddress(diffs.get(), &args.renderWindow, 1, p.x, p.y);
                 oldScore += *diffPtr;
-                scratchDiffPtr = scratchDiffs.get() + offset;
+                scratchDiffPtr = pixelAddress(scratchDiffs.get(), &args.renderWindow, 1, p.x, p.y);
                 *scratchDiffPtr = 0;
-                trgPix = (float*)trgImg->getPixelAddress(x, y);
+                trgPix = (float*)trgImg->getPixelAddress(p.x, p.y);
                 bilinear(
                     srcX
                     ,srcY
@@ -230,35 +259,106 @@ void EstimateOffsetMapPlugin::render(const OFX::RenderArguments &args)
             }
         }
 
-        if (newScore < oldScore) {
-            for (auto y=bounds.y1; y < bounds.y2; y++) {
-                if (abort()) {return;}
-                for (auto x=bounds.x1; x < bounds.x2; x++) {
-                    auto offset = rndWidth * (y - args.renderWindow.y1) + (x - args.renderWindow.x1);
-                    changedPtr = changed.get() + offset;
-                    if (!*changedPtr) {
+        // std::cout << newScore << " " << oldScore << std::endl;
+        if (newScore >= oldScore) {
+            continue;
+        }
+
+        // propagate effects
+        for (auto j=0; j < touched.size(); j++) {
+            if (abort()) {return;}
+            auto p = touched[j];
+            scratchDrawPtr = pixelAddress(scratchDraws.get(), &args.renderWindow, 2, p.x, p.y);
+            quad.edges[0].p.x = scratchDrawPtr[0];
+            quad.edges[0].p.y = scratchDrawPtr[1];
+            auto sumP = quad.edges[0].p;
+            for (auto k=0; k < 4; k++) {
+                locked.clear();
+                auto curP = p;
+                auto skip = false;
+                for (auto l=0; l < 3; l++) {
+                    auto rot = (k + l) % 4;
+                    curP.x += rot < 3 ? 1 - rot : 0;
+                    curP.y += rot > 0 ? 2 - rot : 0;
+                    if (
+                        curP.x < args.renderWindow.x1
+                        || curP.x >= args.renderWindow.x2
+                        || curP.y < args.renderWindow.y1
+                        || curP.y >= args.renderWindow.y2
+                    ) {
+                        skip = true;
+                        break;
+                    }
+                    if (touchedSet.find(curP) != touchedSet.end()) {
+                        locked.insert(l + 1);
+                        if (locked.size() > 2) {
+                            skip = true;
+                            break;
+                        }
+                        scratchDrawPtr = pixelAddress(scratchDraws.get(), &args.renderWindow, 2, curP.x, curP.y);
+                        sumP.x += scratchDrawPtr[0];
+                        sumP.y += scratchDrawPtr[1];
+                    } else {
+                        scratchDrawPtr = pixelAddress(draws.get(), &args.renderWindow, 2, curP.x, curP.y);
+                    }
+                    quad.edges[l + 1].p.x = scratchDrawPtr[0];
+                    quad.edges[l + 1].p.y = scratchDrawPtr[1];
+                }
+                if (skip) {
+                    continue;
+                }
+                quad.initialise();
+                if (quad.isValid()) {
+                    continue;
+                }
+                auto avgP = sumP / locked.size();
+                curP = p;
+                for (auto l=0; l < 3; l++) {
+                    auto rot = (k + l) % 4;
+                    curP.x += rot < 3 ? 1 - rot : 0;
+                    curP.y += rot > 0 ? 2 - rot : 0;
+                    if (locked.find(l + 1) != locked.end()) {
                         continue;
                     }
-                    *changedPtr = false;
-                    scratchDrawPtr = scratchDraws.get() + offset * 2;
-                    drawsPtr = draws.get() + offset * 2;
-                    drawsPtr[0] = scratchDrawPtr[0];
-                    drawsPtr[1] = scratchDrawPtr[1];
-                    scratchDiffPtr = scratchDiffs.get() + offset;
-                    diffPtr = diffs.get() + offset;
-                    *diffPtr = *scratchDiffPtr;
+                    quad.edges[l + 1].p = avgP;
+                    touched.push_back(curP);
+                    touchedSet.insert(curP);
+                    scratchDrawPtr = pixelAddress(scratchDraws.get(), &args.renderWindow, 2, curP.x, curP.y);
+                    scratchDrawPtr[0] = quad.edges[l + 1].p.x;
+                    scratchDrawPtr[1] = quad.edges[l + 1].p.y;
+                    scratchDiffPtr = pixelAddress(scratchDiffs.get(), &args.renderWindow, 1, curP.x, curP.y);
+                    *scratchDiffPtr = 0;
+                    trgPix = (float*)trgImg->getPixelAddress(curP.x, curP.y);
+                    bilinear(
+                        scratchDrawPtr[0]
+                        ,scratchDrawPtr[1]
+                        ,srcImg.get()
+                        ,srcPix.get()
+                        ,comps
+                        ,blackOutside
+                    );
+                    for (auto c=0; c < comps; c++) {
+                        *scratchDiffPtr += std::abs(trgPix[c] - srcPix.get()[c]);
+                    }
+                    newScore += *scratchDiffPtr;
+                    diffPtr = pixelAddress(diffs.get(), &args.renderWindow, 1, curP.x, curP.y);
+                    oldScore += *diffPtr;
                 }
             }
-        } else {
-            // wipe changed
-            for (auto y=bounds.y1; y < bounds.y2; y++) {
-                if (abort()) {return;}
-                for (auto x=bounds.x1; x < bounds.x2; x++) {
-                    auto offset = rndWidth * (y - args.renderWindow.y1) + (x - args.renderWindow.x1);
-                    changedPtr = changed.get() + offset;
-                    *changedPtr = false;
-                }
-            }
+        }
+
+        if (newScore >= oldScore) {
+            continue;
+        }
+
+        for (auto p : touched) {
+            scratchDrawPtr = pixelAddress(scratchDraws.get(), &args.renderWindow, 2, p.x, p.y);
+            drawsPtr = pixelAddress(draws.get(), &args.renderWindow, 2, p.x, p.y);
+            drawsPtr[0] = scratchDrawPtr[0];
+            drawsPtr[1] = scratchDrawPtr[1];
+            scratchDiffPtr = pixelAddress(scratchDiffs.get(), &args.renderWindow, 1, p.x, p.y);
+            diffPtr = pixelAddress(diffs.get(), &args.renderWindow, 1, p.x, p.y);
+            *diffPtr = *scratchDiffPtr;
         }
     }
 
